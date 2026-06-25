1ea95223cde011c7352d82972d8e2225c46d3be0
Ilya Hvostenko <ilyajavadev@gmail.com>
2026-06-25 07:17:35 +0300

    home fix

--- app/root.tsx
+++ app/root.tsx
@@ -27,7 +27,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+      <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -35,11 +35,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      {children}
+      <ScrollRestoration />
+      <Scripts />
       </body>
-    </html>
+      </html>
   );
 }
 
@@ -55,21 +55,21 @@ export default function App() {
 
   const refreshAuth=async ()=>{
 
-try {
-  const user=await getCurrentUser()
-  setAuthState({
-    isSignedIn:!!user,
-    userName:user?.username || null,
-    userId:user?.uuid || null
+    try {
+      const user=await getCurrentUser()
+      setAuthState({
+        isSignedIn:!!user,
+        userName:user?.username || null,
+        userId:user?.uuid || null
 
-  })
+      })
 
-  return !!user;
+      return !!user;
 
-}catch {
-  setAuthState(DEFAULT_AUTH_STATE)
-  return false;
-}
+    }catch {
+      setAuthState(DEFAULT_AUTH_STATE)
+      return false;
+    }
 
   }
 
@@ -95,11 +95,11 @@ try {
   return <main className='min-h-screen bg-background text-foreground relative z-10'>
 
     <Outlet
-    context={
-      {
-        ...authState,refreshAuth,signIn,signOut,
-      }
-    }
+        context={
+          {
+            ...authState,refreshAuth,signIn,signOut,
+          }
+        }
 
     />
   </main>
@@ -115,23 +115,23 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   if (isRouteErrorResponse(error)) {
     message = error.status === 404 ? "404" : "Error";
     details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
+        error.status === 404
+            ? "The requested page could not be found."
+            : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;
     stack = error.stack;
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+      <main className="pt-16 p-4 container mx-auto">
+        <h1>{message}</h1>
+        <p>{details}</p>
+        {stack && (
+            <pre className="w-full p-4 overflow-x-auto">
           <code>{stack}</code>
         </pre>
-      )}
-    </main>
+        )}
+      </main>
   );
 }
